9229480c2ec0faef74300449acf2e52ea783e860
AleDv-1 <alessiodevincenzi@outlook.it>
2022-11-17 15:03:20 +0100

    Test Corretti

--- src/test/EuroTest.java
+++ src/test/EuroTest.java
@@ -1,35 +1,53 @@
 package test;
+import code.Business_logic.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class EuroTest {
     @Test
     public void testGetValore() {
-
+        final Euro verifica = new Euro (60);
+        assertEquals (6000,(int)verifica.getValore ());
     }
 
     @Test
     public void testMinoreDi() {
-
+        final Euro a = new Euro (5);
+        final Euro b = new Euro (2);
+        assertEquals(true,b.minoreDi (a));
     }
 
     @Test
     public void testSomma() {
-
+        final Euro a = new Euro (10);
+        final Euro b = new Euro (5);
+        b.somma (a);
+        assertEquals(1500,b.getValore());
     }
 
     @Test
     public void testSottrai() {
 
+        final Euro a = new Euro (10);
+        final Euro b = new Euro (5);
+        a.sottrai(b);
+        assertEquals(500,b.getValore());
     }
 
     @Test
     public void testStampa() {
 
+        final Euro a = new Euro (50);
+        final String k = "50.0 euro";
+        assertEquals(k, a.stampa());
     }
 
     @Test
     public void testUgualeA() {
 
+        final Euro a = new Euro (5);
+        final Euro b = new Euro (5);
+        assertEquals(true,b.ugualeA(a));
     }
 }
